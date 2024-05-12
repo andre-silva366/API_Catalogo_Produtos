38b653394c500ca39dbc5fa96f839c269a98c0d5
AndreFirstOrDefault <andre.java.ads.2022@gmail.com>
2024-05-12 17:03:22 -0300

    Criação de projeto de teste xUnit

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302d245bee434b60efb8056a7760efc76a42af9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d554e758ff8a0f2384aff82f241b3657b61e065d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
